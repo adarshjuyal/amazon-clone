d7530c63043a6b6cd8b48173d921daa5773ef3df
adarshjuyal14 <adarshjuyal14@gmail.com>
2024-02-05 21:26:49 +0530

    import products from product.js

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,4 +1,5 @@
 import {cart } from '../data/cart.js';
+import { products } from '../data/products.js';
 let productsHtml='';
 products.forEach((prodcut) => {
    productsHtml+=` <div class="product-container">
