1b93bfb544ccaa1bdf322e23b1c867a94c30ccc9
adarshjuyal14 <adarshjuyal14@gmail.com>
2024-02-06 21:48:51 +0530

    Save cart in local storage

--- data/cart.js
+++ data/cart.js
@@ -1,5 +1,9 @@
 
-export let cart = [{
+export let cart = JSON.parse(localStorage.getItem('cart'));
+
+if(!cart){
+    cart=
+    [{
     productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
     quantity:2
 },{
@@ -7,6 +11,12 @@ export let cart = [{
     quantity:1
 }];
 
+}
+
+
+function saveToStorage(){
+    localStorage.setItem('cart',JSON.stringify(cart));
+}
 export function addToCart(productId) {
     let matchingItem;
 
@@ -27,7 +37,7 @@ export function addToCart(productId) {
             quantity: 1
         });
     }
-
+saveToStorage();
 
 }
 
@@ -39,5 +49,6 @@ export function removeFromCart(productId){
             }
     });
     cart=newCart;
+saveToStorage();
     
 }
